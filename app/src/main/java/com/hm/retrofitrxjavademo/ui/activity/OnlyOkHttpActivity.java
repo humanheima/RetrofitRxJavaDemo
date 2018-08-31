ef88d6e180a6d6c57ca8e482e3afa47230dd361d
humanheima <humanheima@gmail.com>
2018-08-31 22:01:26 +0800

    Modify OnlyOkHttpActivity

--- app/src/main/java/com/hm/retrofitrxjavademo/ui/activity/OnlyOkHttpActivity.java
+++ app/src/main/java/com/hm/retrofitrxjavademo/ui/activity/OnlyOkHttpActivity.java
@@ -4,14 +4,26 @@ import com.hm.retrofitrxjavademo.R;
 import com.hm.retrofitrxjavademo.databinding.ActivityOnlyOkHttpBinding;
 import com.hm.retrofitrxjavademo.ui.base.BaseActivity;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 /**
  * Created by dmw on 2018/8/31.
  * Desc:
  */
 public class OnlyOkHttpActivity extends BaseActivity<ActivityOnlyOkHttpBinding> {
 
-    @Override
+    OkHttpClient client = new OkHttpClient();
 
+
+    @Override
     protected int bindLayout() {
         return R.layout.activity_only_ok_http;
     }
@@ -19,5 +31,42 @@ public class OnlyOkHttpActivity extends BaseActivity<ActivityOnlyOkHttpBinding> 
     @Override
     protected void initData() {
 
+    }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public void run() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://publicobject.com/helloworld.txt")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(responseBody.string());
+                }
+            }
+        });
     }
 }
